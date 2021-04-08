70b91f2602f5cbf5cb7e121904061cd3b8353248
eggyatma2908 <eggyatmariansyah@gmail.com>
2021-04-08 09:18:32 +0700

    Updated

---
+++ Day2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <title>Home</title>
+</head>
+<body>
+    <div class="container mt-3 mb-3">
+        <h1>Welcome</h1>
+        <a href="./views/list.php" class="btn btn-primary">My Product</a>
+    </div>
+    
+    
+</body>
+</html>
